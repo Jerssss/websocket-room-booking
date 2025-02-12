ccc702664445fdce1bac7ca94d05cbdd00c17023
Anjelo Esperanzate <2240084@slu.edu.ph>
2025-02-12 20:15:16 +0800

    Added comments

--- src/main/java/client/admin/controller/ModifyTerminalStatusController.java
+++ src/main/java/client/admin/controller/ModifyTerminalStatusController.java
@@ -1,4 +1,11 @@
 package client.admin.controller;
 
 public class ModifyTerminalStatusController {
+
+
+    //TODO to create a button in the column
+    //createButton()
+    //setEditInformationColumn()
+    //handleEditButtonClick()
+
 }
