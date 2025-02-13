4f438c9d4ca94838e3ca9a24780b797b97e2b3f8
Gab <alysongabrielflores@gmail.com>
2025-02-13 21:19:11 +0800

    updated gui room card generation

--- src/main/java/client/admin/view/ReservationApprovalView.java
+++ src/main/java/client/admin/view/ReservationApprovalView.java
@@ -1,4 +1,9 @@
 package client.admin.view;
 
+import client.admin.controller.ReservationApprovalController;
+import client.admin.model.ReservationApprovalModel;
+
 public class ReservationApprovalView {
+    public ReservationApprovalView(ReservationApprovalController reservationApprovalController, ReservationApprovalModel reservation) {
+    }
 }
