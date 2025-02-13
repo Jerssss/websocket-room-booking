863455f988f25e709747aebe82763680c3133351
Bryant Casey Pangilinan <2153766@slu.edu.ph>
2025-02-13 15:09:56 +0800

    Implemented the reservation approval and rejection logic (initial).

--- src/main/java/client/admin/controller/ReservationApprovalController.java
+++ src/main/java/client/admin/controller/ReservationApprovalController.java
@@ -1,4 +1,23 @@
-package client.admin.controller;
-
 public class ReservationApprovalController {
+
+    private ReservationApprovalModel reservation;
+    private ReservationApprovalView view;
+
+    public ReservationApprovalController(ReservationApprovalModel reservation) {
+        this.reservation = reservation;
+        this.view = new ReservationApprovalView(this, reservation);
+    }
+
+    public void approveReservation() {
+        reservation.approve();
+    }
+
+    public void rejectReservation() {
+        reservation.reject();
+    }
+
+    public static void main(String[] args) {
+        ReservationApprovalModel reservation = new ReservationApprovalModel(1, "Bryant Pangilinan", "2025-02-13");
+        new ReservationApprovalController(reservation);
+    }
 }
