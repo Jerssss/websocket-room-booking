fdac62173b175af111a6e5fa93c00b2b0c06d2e7
Giana <2241883@slu.edu.ph>
2025-02-14 18:19:31 +0800

    Created the Controller of the ModifyTerminalStatus for the function of displaying results in a table

--- src/main/java/client/admin/controller/ModifyTerminalStatusController.java
+++ src/main/java/client/admin/controller/ModifyTerminalStatusController.java
@@ -1,20 +1,38 @@
 package client.admin.controller;
-import client.admin.model.ModifyTerminalStatusModel;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import client.admin.view.ModifyTerminalStatusView;
 
 public class ModifyTerminalStatusController {
-    private final ModifyTerminalStatusView modifyTerminalStatusView;
-    private final ModifyTerminalStatusModel modifyTerminalStatusModel;
 
-    public ModifyTerminalStatusController(ModifyTerminalStatusView modifyTerminalStatusView, ModifyTerminalStatusModel modifyTerminalStatusModel) {
-        this.modifyTerminalStatusView = modifyTerminalStatusView;
-        this.modifyTerminalStatusModel = modifyTerminalStatusModel;
+    private ModifyTerminalStatusView view;
+
+    // Constructor to set the view (in case you need it to access view methods)
+    public ModifyTerminalStatusController(ModifyTerminalStatusView view) {
+        this.view = view;
     }
 
+    // Set the action for the Search button
+    public void handleSearchButtonAction() {
+        view.setActionSearchButton(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // Add the logic for search button click
+                System.out.println("Search button clicked!");
+            }
+        });
+    }
 
-    //TODO to create a button in the column
-    //createButton()
-    //setEditInformationColumn()
-    //handleEditButtonClick()
+    // Set the action for the Save Changes button
+    public void handleSaveChangesButtonAction() {
+        view.setActionSaveChangesButton(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // Add the logic for save changes button click
+                System.out.println("Save Changes button clicked!");
+            }
+        });
+    }
 
 }
