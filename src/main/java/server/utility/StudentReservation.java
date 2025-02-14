bc1e8bb049313eec4d622309730fdf832e7e9f6b
Sebasthian Molina <2242541@slu.edu.ph>
2025-02-14 23:35:05 +0800

    Created a StudentReservation class and ViewStudentReservationController to be used for the MVC of ViewStudentReservation to display extracted information on the xml to the table

---
+++ src/main/java/server/utility/StudentReservation.java
@@ -0,0 +1,59 @@
+package server.utility;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class StudentReservation {
+    private StringProperty reservationId;
+    private StringProperty terminalId;
+    private StringProperty terminalRoom;
+    private StringProperty date;
+    private StringProperty terminalStatus;
+
+    // Constructor
+    public StudentReservation(String reservationId, String terminalId, String terminalRoom, String date, String terminalStatus) {
+        this.reservationId = new SimpleStringProperty(reservationId);
+        this.terminalId = new SimpleStringProperty(terminalId);
+        this.terminalRoom = new SimpleStringProperty(terminalRoom);
+        this.date = new SimpleStringProperty(date);
+        this.terminalStatus = new SimpleStringProperty(terminalStatus);
+    }
+
+    // Getters
+    public String getReservationId() {
+        return reservationId.get();
+    }
+
+    public StringProperty reservationIdProperty() {
+        return reservationId;
+    }
+
+    public String getTerminalId() {
+        return terminalId.get();
+    }
+
+    public StringProperty terminalIdProperty() {
+        return terminalId;
+    }
+
+    public String getTerminalRoom() {
+        return terminalRoom.get();
+    }
+
+    public StringProperty terminalRoomProperty() {
+        return terminalRoom;
+    }
+    public String getDate() {
+        return date.get();
+    }
+    public StringProperty dateProperty() {
+        return date;
+    }
+    public String getTerminalStatus() {
+        return terminalStatus.get();
+    }
+
+    public StringProperty terminalStatusProperty() {
+        return terminalStatus;
+    }
+}
