5d871fae675e8366f5301fe4a5ad160abe6dd65f
Sebasthian Molina <2242541@slu.edu.ph>
2025-02-14 23:32:53 +0800

    Created the ViewStudentReservationsController for functions

--- src/main/java/client/admin/controller/ViewStudentReservationsController.java
+++ src/main/java/client/admin/controller/ViewStudentReservationsController.java
@@ -1,4 +1,26 @@
 package client.admin.controller;
 
+import client.admin.view.ViewStudentReservationsView;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
 public class ViewStudentReservationsController {
+
+    private ViewStudentReservationsView view;
+
+    // Constructor to set the view (in case you need it to access view methods)
+    public ViewStudentReservationsController(ViewStudentReservationsView view) {
+        this.view = view;
+    }
+
+    // Set the action for the Search button
+    public void handleSearchButtonAction() {
+        view.setActionSearchButton(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // Add the logic for search button click
+                System.out.println("Search button clicked!");
+            }
+        });
+    }
 }
