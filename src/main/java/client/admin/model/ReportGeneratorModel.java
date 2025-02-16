e357007a0723e73cd57b2d169ae91228e6283947
Giana <2241883@slu.edu.ph>
2025-02-16 17:29:22 +0800

    Created a method to call the processor that loads data from the log xml

--- src/main/java/client/admin/model/ReportGeneratorModel.java
+++ src/main/java/client/admin/model/ReportGeneratorModel.java
@@ -1,4 +1,35 @@
 package client.admin.model;
 
+import client.utility.ServerConnectionManager;
+import client.utility.ServerConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import server.utility.LogReport;
+import server.admin.ReportGeneratorProcessor;
+
+import java.io.IOException;
+import java.util.List;
+
 public class ReportGeneratorModel {
+    private ServerConnection serverConnection;
+
+    public ReportGeneratorModel () {
+        try {
+            serverConnection = ServerConnectionManager.getConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ObservableList<LogReport> loadLogsFromServer() {
+        if (serverConnection != null) {
+            List<LogReport> logs = ReportGeneratorProcessor.parseLogXML();
+            if (logs == null) {
+                System.out.println("No terminal data received from server!");
+                return FXCollections.observableArrayList();
+            }
+            return FXCollections.observableArrayList(logs);
+        }
+        return FXCollections.observableArrayList();
+    }
 }
